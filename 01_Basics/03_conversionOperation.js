362ed2566f6d1ffcbfe53b4e26525dac131264ab
manishjungari <143917573+manishjungari@users.noreply.github.com>
2024-01-02 15:33:36 +0000

    update

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -32,4 +32,48 @@ console.log(booleanIsLoggedIn);
 let someNumber = 53;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);stringNumber
+console.log(stringNumber);
+
+// ****************************************Operations*************************************************************
+
+// -ve value 
+
+let value =3 
+let negValue = -value
+console.log(negValue);
+
+// basics math operations 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// combine string 
+
+let str1 = "Hello"
+let str2 = " Hitesh"
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 3);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
