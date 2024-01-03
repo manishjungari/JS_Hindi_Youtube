926876dc20436468b98ea786ef0053b235a4b5ff
manishjungari <143917573+manishjungari@users.noreply.github.com>
2024-01-03 15:21:58 +0000

    DataType Summary

---
+++ 01_Basics/datatype-summary.js
@@ -0,0 +1,47 @@
+// Data Types 
+
+// Primitive 
+
+// 7-TYPES:-String , Number , Boolean , Null, Undefined , Symbol , BigInt
+
+// Reference (Non premitive data type)\
+
+// Array , Objects, Functions
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 321547854587994498225656n
+
+
+// Array 
+
+const heros = ["Shaktiman", "Spiderman", "Batman"]
+
+
+// objects 
+
+let myobj = {
+
+    name: "hitesh",
+    age : "22",
+    address: "Nagpur",
+}
+
+
+// Functions 
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+console.log(typeof bigNumber );
