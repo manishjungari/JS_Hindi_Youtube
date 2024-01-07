f19645ccbac1dd76f399dc22eb9d69c5d171780c
manishjungari <143917573+manishjungari@users.noreply.github.com>
2024-01-07 14:51:38 +0000

    type array

--- 02Basics/arrays.js
+++ 02Basics/arrays.js
@@ -51,3 +51,48 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1 , 3)
 console.log("C ", myArr);
 console.log(myn2);
+
+// ***********************************************************************************************
+// Arrays02
+
+ const marvel_heros = ["thor", " Ironman", "Spiderman"]
+ const dc_heros = ["Superman", "flash", "batman"]
+
+// // push (join two arrays)
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// concat (join two arrays)
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
+
+// *Spread operator (advance method to join array)
+
+const all_new_heros = [...marvel_heros , ...dc_heros]
+console.log(all_new_heros);
+
+//Another array ex:-all combine value in one array
+
+const another_array = [1 , 2 , 3 ,[4 , 5 , 6], 7 , [6 , 7 ,[4 , 5 ]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+// Is array  (check type)
+ 
+console.log(Array.isArray("Manish"));
+
+// From array
+console.log(Array.from("Manish"));
+
+console.log(Array.from({name: "Manish"})); // interesting
+
+//  Of array
+
+let score1 = 100
+let score2 = 200
+let score3 =300 
+
+console.log(Array.of(score1, score2, score3));
