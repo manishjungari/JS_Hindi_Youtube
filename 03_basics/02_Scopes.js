02a44c81511a28ad59d6af260d680481d939543c
manishjungari <143917573+manishjungari@users.noreply.github.com>
2024-01-11 18:01:36 +0000

    adv function

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -18,3 +18,66 @@ if(true){
 
 }
 console.log(a)
+
+
+// Nested Function(closer function) (function ke andar function)(child function can take a variable access of parent function)
+
+function one(){
+const username = "hitesh"
+
+function two(){
+const website = "youtube"
+console.log(username);
+
+console.log(website);
+}
+
+two()
+
+}
+
+one()
+
+
+
+// ex nested fun with if else loop (with error)
+
+if(true){
+const username ="Hitesh"
+if(username=== "Hitesh"){
+    const website = " Youtube"
+    console.log(username + website);
+}
+// console.log(website);
+}
+// console.log(username);
+
+
+// or (correct)
+
+if(true){
+const username ="Hitesh"
+if(username=== "Hitesh"){
+    const website = " Youtube"
+    console.log(username + website);
+    console.log(website);
+}
+console.log(username);
+}
+
+// ***************************Interesting**************************************
+// function declaration
+
+function addone(num){
+    return num +1
+}
+
+console.log(addone(5))
+
+// Function declaration with hold in variable
+
+ const addtwo = function(num){
+    return num +2
+}
+
+console.log(addone(5))
